dd67d0fbcadaf7bdb576dfa0f5acb45161b25b8c
AlexPhill <alexander.phillips@ontariotechu.net>
2023-02-13 19:07:29 -0500

    made unity scene

---
+++ Assets/Shaders/Blending/BlendingShader.shader
@@ -0,0 +1,14 @@
+Shader "Alexander/BlendingShader"
+{
+    Properties
+    {
+        _MainTex("Texture", 2D) = "black" {}
+    }
+        SubShader{
+        Tags {"Queue" = "Transparent"}
+        Blend DstColor Zero
+        Pass{
+                SetTexture[_MainTex] {combine texture}
+        } 
+    }
+}
---
+++ Assets/Shaders/Decals/DecalOnOff.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class DecalOnOff : MonoBehaviour
+{
+    Material mat;
+    bool showDecal = false;
+
+    private void OnMouseDown()
+    {
+        showDecal = !showDecal;
+        if (showDecal)
+            mat.SetFloat("_ShowDecal", 1);
+        else
+            mat.SetFloat("_ShowDecal", 0);
+    }
+    void Start()
+    {
+        mat = this.GetComponent<Renderer>().sharedMaterial;
+    }
+}
